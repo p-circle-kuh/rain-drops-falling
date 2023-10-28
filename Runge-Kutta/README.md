3c33b5b06b85634c7639ab2e82b36d6f2b3095ad
Hiroki <>
2023-10-28 14:17:23 +0900

    Add Runge-Kutta

---
+++ Runge-Kutta/README.md
@@ -0,0 +1,15 @@
+# Raindrops Falling with Numerical Integration
+
+Solve the Equation Of Motion for a falling raindrop with air resistance:
+
+```
+dv/dt = g - k * v
+```
+
+### Environment
+
+- Python 3.11.0
+
+### Reference
+- https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html#scipy.integrate.solve_ivp
+
---
+++ Runge-Kutta/Runge-Kutta.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+# Initial Values
+T_INITIAL = 0  # [s]
+V_INITIAL = 0  # [m/s]
+
+# Params for Phys.
+k = 1  # [1/s]
+g = 9.8  # [m/s]
+
+# Params for Calc.
+T_FINAL = 10  # [s]
+T_STEP = 0.1  # [s]
+
+
+def derivative(t, v):  # Solver needs the variable t
+    return g - k * v
+
+
+def plot(t, v):
+    plt.figure(figsize=(8, 6))
+    plt.plot(t, v, '-o')
+    plt.hlines([g], T_INITIAL, T_FINAL, "red", linestyles='dashed')
+    plt.xlim([T_INITIAL, T_FINAL])
+    plt.xlabel('t [s]')
+    plt.ylabel('v (downward) [m/s]')
+    plt.title(f'Velocity of a falling raindrop (g={g} [m/s^2], k={k} [1/s])')
+    plt.show()
+
+
+def main():
+    n_steps = (T_FINAL - T_INITIAL) / T_STEP
+    t_eval = T_INITIAL + np.arange(0, n_steps) * T_STEP
+    result = solve_ivp(derivative, [T_INITIAL, T_FINAL], [V_INITIAL], t_eval=t_eval)
+
+    if not result.success:
+        print('Failed to solve!')
+        return
+
+    plot(result.t, result.y[0])  # solve_ivp returns t and multi-dimensional y 
+
+
+if __name__ == '__main__':
+    main()
